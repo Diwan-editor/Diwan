b0fe4a9e1cc0b302ba65dbe3708131f932371972
Ayman El Hasbi <aelhasbi11@gmail.com>
2024-09-01 15:33:20 +0100

    small fix 2

--- diwan/src/main.rs
+++ diwan/src/main.rs
@@ -13,10 +13,25 @@ async fn main() -> Result<(), Error> {
     let mut typed_text = String::new();
     let mut typed_text2 = String::new();
     let main_screen = MainScreen::new_with_widget(&mut typed_text)?;
-    let main_screen2 = MainScreen::new_with_widget(&mut typed_text2)?;
     let ui = main_screen.setup_ui();
-    main_screen.main_event_loop(ui)?;
-    let widgetId = ui.add(Some(main_screen2));
+    let mut arc_ui = Arc::new(Mutex::new(&ui));
+
+    tokio::spawn(move {
+        let ui = *arc_ui.lock().unwrap();
+        main_screen.main_event_loop(ui)?;
+    });
+
+    let main_screen2 = MainScreen::new_with_widget(&mut typed_text2)?;
+
+    tokio::spawn(move {
+        let ui = *arc_ui.lock().unwrap();
+        let widgetId = ui.add(Some(main_screen2));
+    });
+
+    // TODO:
+    // 1. Create a new thread that holds WidgetId;
+    // 2. Ui must be in a separate thread ;
+    // 3. Read a bit about Discovering threads , u may want to read about Observable pattern !
 
     println!("widgetId: {}", widgetId);
     dbg!(&ui);
