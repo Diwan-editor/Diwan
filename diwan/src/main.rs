662e16452a65908cacfd8ed87c06a5fe6eaf80a1
Ayman El Hasbi <aelhasbi11@gmail.com>
2024-09-01 15:14:21 +0100

    small fix

--- diwan/src/main.rs
+++ diwan/src/main.rs
@@ -11,10 +11,16 @@ use screen::MainScreen;
 async fn main() -> Result<(), Error> {
 
     let mut typed_text = String::new();
+    let mut typed_text2 = String::new();
     let main_screen = MainScreen::new_with_widget(&mut typed_text)?;
+    let main_screen2 = MainScreen::new_with_widget(&mut typed_text2)?;
     let ui = main_screen.setup_ui();
     main_screen.main_event_loop(ui)?;
+    let widgetId = ui.add(Some(main_screen2));
 
+    println!("widgetId: {}", widgetId);
+    dbg!(&ui);
     println!("The text you entered: {}", typed_text);
+
     Ok(())
 }
